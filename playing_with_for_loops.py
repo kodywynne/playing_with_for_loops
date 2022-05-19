173b4a98c7b15493d581e8483ea0a1510f7b929c
kodywynne <65366132+kodywynne@users.noreply.github.com>
2022-05-18 20:25:10 -0700

    Create playing_with_for_loops.py
    Playing with loops

---
+++ playing_with_for_loops.py
@@ -0,0 +1,21 @@
+# printing numbers in a list
+for i in [5,10,2,53,32,9,4,5]:
+    print(i)
+
+#################################
+
+#Running through friends list each item at a time and counting
+
+count=0
+
+friends = ["Johnny", "Kim", "Becky", "Kody"]
+for friend in friends:
+    count += 1
+    print(count, friend) 
+    print(friends[1:3])
+    
+#######################################    
+    
+child_ages = [4, 2, 5, 3, 5, 3, 4, 2]    
+for age in child_ages:
+    print(age)
